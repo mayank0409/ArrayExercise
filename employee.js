65c57366771873fb5f88b60b9ad443e23a2765c5
Mayank Mahajan <mayankmahajanbb@gmail.com>
2019-08-04 14:44:19 +0530

    Initial Commit

---
+++ employee.js
@@ -0,0 +1,452 @@
+var employees = [
+  {
+    id: 1,
+    first_name: "Meriel",
+    last_name: "Dunderdale",
+    email: "mdunderdale0@telegraph.co.uk",
+    gender: "Female",
+    department: "Sales",
+    age: 84
+  },
+  {
+    id: 2,
+    first_name: "Flin",
+    last_name: "Mungan",
+    email: "fmungan1@chicagotribune.com",
+    gender: "Male",
+    department: "Marketing",
+    age: 82
+  },
+  {
+    id: 3,
+    first_name: "Derry",
+    last_name: "Keming",
+    email: "dkeming2@webmd.com",
+    gender: "Male",
+    department: "Sales",
+    age: 67
+  },
+  {
+    id: 4,
+    first_name: "Gizela",
+    last_name: "Stockman",
+    email: "gstockman3@twitpic.com",
+    gender: "Female",
+    department: "Support",
+    age: 76
+  },
+  {
+    id: 5,
+    first_name: "Ransell",
+    last_name: "Slorach",
+    email: "rslorach4@ask.com",
+    gender: "Male",
+    department: "Business Development",
+    age: 90
+  },
+  {
+    id: 6,
+    first_name: "Geoff",
+    last_name: "Fockes",
+    email: "gfockes5@msu.edu",
+    gender: "Male",
+    department: "Legal",
+    age: 100
+  },
+  {
+    id: 7,
+    first_name: "Ki",
+    last_name: "Berthelet",
+    email: "kberthelet6@creativecommons.org",
+    gender: "Female",
+    department: "Support",
+    age: 71
+  },
+  {
+    id: 8,
+    first_name: "Allyn",
+    last_name: "Arnholtz",
+    email: "aarnholtz7@jigsy.com",
+    gender: "Female",
+    department: "Accounting",
+    age: 49
+  },
+  {
+    id: 9,
+    first_name: "Eachelle",
+    last_name: "Varrow",
+    email: "evarrow8@imageshack.us",
+    gender: "Female",
+    department: "Engineering",
+    age: 50
+  },
+  {
+    id: 10,
+    first_name: "Sydney",
+    last_name: "Mayoh",
+    email: "smayoh9@uol.com.br",
+    gender: "Male",
+    department: "Support",
+    age: 66
+  },
+  {
+    id: 11,
+    first_name: "Maurizio",
+    last_name: "Euesden",
+    email: "meuesdena@nba.com",
+    gender: "Male",
+    department: "Support",
+    age: 67
+  },
+  {
+    id: 12,
+    first_name: "Guillemette",
+    last_name: "Raynes",
+    email: "graynesb@jiathis.com",
+    gender: "Female",
+    department: "Sales",
+    age: 61
+  },
+  {
+    id: 13,
+    first_name: "Ian",
+    last_name: "Kimbrey",
+    email: "ikimbreyc@blogspot.com",
+    gender: "Male",
+    department: "Research and Development",
+    age: 25
+  },
+  {
+    id: 14,
+    first_name: "Jarvis",
+    last_name: "Swalwel",
+    email: "jswalweld@imageshack.us",
+    gender: "Male",
+    department: "Sales",
+    age: 74
+  },
+  {
+    id: 15,
+    first_name: "Chrissy",
+    last_name: "Pitblado",
+    email: "cpitbladoe@cmu.edu",
+    gender: "Female",
+    department: "Training",
+    age: 59
+  },
+  {
+    id: 16,
+    first_name: "Leupold",
+    last_name: "Monckton",
+    email: "lmoncktonf@reddit.com",
+    gender: "Male",
+    department: "Research and Development",
+    age: 34
+  },
+  {
+    id: 17,
+    first_name: "Atalanta",
+    last_name: "Peasegod",
+    email: "apeasegodg@youtu.be",
+    gender: "Female",
+    department: "Marketing",
+    age: 94
+  },
+  {
+    id: 18,
+    first_name: "Eugenie",
+    last_name: "Coie",
+    email: "ecoieh@cargocollective.com",
+    gender: "Female",
+    department: "Services",
+    age: 75
+  },
+  {
+    id: 19,
+    first_name: "Myrwyn",
+    last_name: "Bidder",
+    email: "mbidderi@cocolog-nifty.com",
+    gender: "Male",
+    department: "Product Management",
+    age: 26
+  },
+  {
+    id: 20,
+    first_name: "Elsinore",
+    last_name: "Shane",
+    email: "eshanej@squidoo.com",
+    gender: "Female",
+    department: "Legal",
+    age: 96
+  },
+  {
+    id: 21,
+    first_name: "Uta",
+    last_name: "Ockenden",
+    email: "uockendenk@cnn.com",
+    gender: "Female",
+    department: "Accounting",
+    age: 85
+  },
+  {
+    id: 22,
+    first_name: "Allan",
+    last_name: "Pawnsford",
+    email: "apawnsfordl@amazon.de",
+    gender: "Male",
+    department: "Business Development",
+    age: 53
+  },
+  {
+    id: 23,
+    first_name: "Faith",
+    last_name: "Fonzone",
+    email: "ffonzonem@wiley.com",
+    gender: "Female",
+    department: "Legal",
+    age: 99
+  },
+  {
+    id: 24,
+    first_name: "Melisent",
+    last_name: "Portsmouth",
+    email: "mportsmouthn@tumblr.com",
+    gender: "Female",
+    department: "Training",
+    age: 32
+  },
+  {
+    id: 25,
+    first_name: "Jabez",
+    last_name: "Pendlenton",
+    email: "jpendlentono@clickbank.net",
+    gender: "Male",
+    department: "Accounting",
+    age: 74
+  },
+  {
+    id: 26,
+    first_name: "Hobard",
+    last_name: "Blacklock",
+    email: "hblacklockp@networkadvertising.org",
+    gender: "Male",
+    department: "Business Development",
+    age: 98
+  },
+  {
+    id: 27,
+    first_name: "Alard",
+    last_name: "Jeavons",
+    email: "ajeavonsq@house.gov",
+    gender: "Male",
+    department: "Engineering",
+    age: 95
+  },
+  {
+    id: 28,
+    first_name: "Bendicty",
+    last_name: "Rawlence",
+    email: "brawlencer@zdnet.com",
+    gender: "Male",
+    department: "Engineering",
+    age: 55
+  },
+  {
+    id: 29,
+    first_name: "Gael",
+    last_name: "Krink",
+    email: "gkrinks@gmpg.org",
+    gender: "Male",
+    department: "Legal",
+    age: 29
+  },
+  {
+    id: 30,
+    first_name: "Ara",
+    last_name: "Axtonne",
+    email: "aaxtonnet@nymag.com",
+    gender: "Male",
+    department: "Legal",
+    age: 62
+  },
+  {
+    id: 31,
+    first_name: "Pauli",
+    last_name: "Rontree",
+    email: "prontreeu@vkontakte.ru",
+    gender: "Female",
+    department: "Business Development",
+    age: 78
+  },
+  {
+    id: 32,
+    first_name: "Matilda",
+    last_name: "Thorsby",
+    email: "mthorsbyv@example.com",
+    gender: "Female",
+    department: "Research and Development",
+    age: 35
+  },
+  {
+    id: 33,
+    first_name: "Elwyn",
+    last_name: "Kainz",
+    email: "ekainzw@edublogs.org",
+    gender: "Male",
+    department: "Accounting",
+    age: 23
+  },
+  {
+    id: 34,
+    first_name: "Austen",
+    last_name: "Edghinn",
+    email: "aedghinnx@cloudflare.com",
+    gender: "Male",
+    department: "Services",
+    age: 89
+  },
+  {
+    id: 35,
+    first_name: "Fidole",
+    last_name: "Janc",
+    email: "fjancy@craigslist.org",
+    gender: "Male",
+    department: "Services",
+    age: 84
+  },
+  {
+    id: 36,
+    first_name: "Raine",
+    last_name: "Sorrill",
+    email: "rsorrillz@nasa.gov",
+    gender: "Female",
+    department: "Sales",
+    age: 26
+  },
+  {
+    id: 37,
+    first_name: "Muire",
+    last_name: "Caville",
+    email: "mcaville10@netlog.com",
+    gender: "Female",
+    department: "Sales",
+    age: 48
+  },
+  {
+    id: 38,
+    first_name: "Renard",
+    last_name: "Cecere",
+    email: "rcecere11@bbc.co.uk",
+    gender: "Male",
+    department: "Services",
+    age: 97
+  },
+  {
+    id: 39,
+    first_name: "Ealasaid",
+    last_name: "Lehrian",
+    email: "elehrian12@github.com",
+    gender: "Female",
+    department: "Business Development",
+    age: 68
+  },
+  {
+    id: 40,
+    first_name: "Karel",
+    last_name: "Borres",
+    email: "kborres13@ocn.ne.jp",
+    gender: "Female",
+    department: "Marketing",
+    age: 86
+  },
+  {
+    id: 41,
+    first_name: "Randy",
+    last_name: "Fansy",
+    email: "rfansy14@e-recht24.de",
+    gender: "Female",
+    department: "Services",
+    age: 96
+  },
+  {
+    id: 42,
+    first_name: "Nilson",
+    last_name: "Skyme",
+    email: "nskyme15@cdbaby.com",
+    gender: "Male",
+    department: "Sales",
+    age: 52
+  },
+  {
+    id: 43,
+    first_name: "Rob",
+    last_name: "Mayston",
+    email: "rmayston16@google.fr",
+    gender: "Male",
+    department: "Support",
+    age: 77
+  },
+  {
+    id: 44,
+    first_name: "Mary",
+    last_name: "Shurlock",
+    email: "mshurlock17@yandex.ru",
+    gender: "Female",
+    department: "Product Management",
+    age: 21
+  },
+  {
+    id: 45,
+    first_name: "Hector",
+    last_name: "Jack",
+    email: "hjack18@wp.com",
+    gender: "Male",
+    department: "Support",
+    age: 39
+  },
+  {
+    id: 46,
+    first_name: "Vere",
+    last_name: "Poyle",
+    email: "vpoyle19@netscape.com",
+    gender: "Female",
+    department: "Accounting",
+    age: 61
+  },
+  {
+    id: 47,
+    first_name: "Virginie",
+    last_name: "Doggart",
+    email: "vdoggart1a@alibaba.com",
+    gender: "Female",
+    department: "Legal",
+    age: 42
+  },
+  {
+    id: 48,
+    first_name: "Melisse",
+    last_name: "Durrett",
+    email: "mdurrett1b@toplist.cz",
+    gender: "Female",
+    department: "Support",
+    age: 33
+  },
+  {
+    id: 49,
+    first_name: "Almira",
+    last_name: "Bartels-Ellis",
+    email: "abartelsellis1c@alexa.com",
+    gender: "Female",
+    department: "Product Management",
+    age: 41
+  },
+  {
+    id: 50,
+    first_name: "Winfield",
+    last_name: "Winsom",
+    email: "wwinsom1d@state.gov",
+    gender: "Male",
+    department: "Product Management",
+    age: 70
+  }
+];
---
+++ filter.js
@@ -0,0 +1,18 @@
+function Q1() {
+  alert("Hi");
+}
+function Q2() {
+  alert("Hi");
+}
+function Q3() {
+  alert("Hi");
+}
+function Q4() {
+  alert("Hi");
+}
+function Q5() {
+  alert("Hi");
+}
+function Q6() {
+  alert("Hi");
+}
